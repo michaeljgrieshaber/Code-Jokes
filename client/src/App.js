530cf9f2024498cec08fe40ab6cfe3051ce680ca
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2021-09-17 08:22:29 -0500

    michael grieshaber

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+const base = process.env.REACT_APP_AIRTABLE_BASE
+const key = process.env.REACT_APP_AIRTABLE_KEY
+
+console.log(base)
+console.log(key)
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
