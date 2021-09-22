eb5325a548b0feed0da17f7a15c7722da9f86b94
Michael Grieshaber <MichaelJGrieshaber@gmail.com>
2021-09-22 17:43:40 -0500

    made joke input an area instead

--- client/src/Components/Form.jsx
+++ client/src/Components/Form.jsx
@@ -2,7 +2,8 @@ export default function Form(props) {
   return (
     <form onSubmit={props.handleSubmit} className="form">
       <label>Joke:</label>
-      <input
+      <textarea
+        className="jokeInput"
         type="text"
         value={props.joke}
         onChange={(e) => props.setJoke(e.target.value)}
@@ -11,6 +12,7 @@ export default function Form(props) {
 
       <label>Author:</label>
       <input
+        className="authorInput"
         type="text"
         value={props.author}
         onChange={(e) => props.setAuthor(e.target.value)}
